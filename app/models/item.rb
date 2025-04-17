b3c4e80568f1fa0cbf686327e8041b84cad4a5bb
Kei-bigginer <new.world.dododo@gmail.com>
2025-04-17 20:44:19 +0900

    モデルの単体テストコード済

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,11 +13,11 @@ class Item < ApplicationRecord
 
   validates :name, presence: true
   validates :description, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :shipping_fee_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :delivery_time_id, presence: true
+  # validates :category_id, presence: true
+  # validates :condition_id, presence: true
+  # validates :shipping_fee_id, presence: true
+  # validates :prefecture_id, presence: true
+  # validates :delivery_time_id, presence: true
   validates :price, presence: true
   validates :price, numericality: {
      only_integer: true, 
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -14,3 +14,4 @@ FactoryBot.define do
     birthday              { Faker::Date.birthday }
   end
 end
+ 
