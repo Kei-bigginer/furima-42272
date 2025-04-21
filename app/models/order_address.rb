51e8f63d0af9d53dd68447838e35974aa3e50f35
Kei-bigginer <new.world.dododo@gmail.com>
2025-04-22 01:10:32 +0900

    現段階でのトークン除く？エラー文確認済み

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,13 +5,18 @@ class OrderAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    #  カード情報未入力時のエラー
+    # validates :token, message: "can't be blank"
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
     validates :city
     validates :address
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Input only numbers' }
   end
-  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
-
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :phone_number, length: { 
+     minimum: 10, 
+      message: 'is too short' 
+    }
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
